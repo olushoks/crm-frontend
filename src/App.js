11cbb39053b00c7f8a0bacb7a60ba88b415ed7db
Olukayode <olukayodeshokunbi@gmail.com>
2021-07-06 21:10:50 -0400

    Cleaned up starter boiler plate code. Also installed react-bootstrap and bootsap libraries

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Hello CRM </Button>
+    </div>
+  );
+}
+
+export default App;
