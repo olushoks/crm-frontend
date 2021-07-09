46fd76f047acce7cb7b50511c761a55f77bc52e8
Olukayode <olukayodeshokunbi@gmail.com>
2021-07-08 23:11:07 -0400

    Created UpdateTicket component to update the status of a ticket

---
+++ src/components/update_ticket/UpdateTicket.component.js
@@ -0,0 +1,29 @@
+import { Form, Button } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+export const UpdateTicket = ({ message, handleChange, handleSubmit }) => {
+  return (
+    <Form onSubmit={handleSubmit}>
+      {/* <Form.Label>Reply</Form.Label> */}
+      <Form.Text>Reply message or update ticket</Form.Text>
+      <Form.Control
+        as="textarea"
+        row="5"
+        name="detail"
+        value={message}
+        onChange={handleChange}
+      />
+      <div className="text-right mt-3 mb-3">
+        <Button variant="info" type="submit">
+          Reply
+        </Button>
+      </div>
+    </Form>
+  );
+};
+
+UpdateTicket.propTypes = {
+  message: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
--- src/pages/ticket/Ticket.page.js
+++ src/pages/ticket/Ticket.page.js
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { BreadCrumb } from "../../components/breadcrumb/BreadCrumb.component";
 import tickets from "../../assets/data/tickets.json";
 import { MessageHistory } from "../../components/message_history/MessageHistory.component";
+import { UpdateTicket } from "../../components/update_ticket/UpdateTicket.component";
 
 const ticket = tickets[0];
 
 export const Ticket = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {}, [message]);
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Form submitted`);
+  };
+
   return (
     <Container>
       <Row>
@@ -25,6 +40,15 @@ export const Ticket = () => {
         <Row className="mt-4">
           <Col>
             <MessageHistory msg={ticket.history} />
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col>
+            <UpdateTicket
+              message={message}
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+            />
           </Col>
         </Row>
       </Row>
